0fabb3dc0cfee61833b834cb907dce71d1babf7d
Sai Srinivas Idimadakala <saisrinivas.idimadakala@gmail.com>
2025-05-19 10:18:45 +0530

    for loops

--- shell-scripting/08-special-variables.sh
+++ shell-scripting/08-special-variables.sh
@@ -2,8 +2,12 @@
 
 echo "printing all variables passed: {$@}"
 echo "All variables passed to the script: $@"
+echo "All Arguments (\$*): $*"
+echo "All Arguments (\$@): $@"
 echo "Number of variables: $#"
+echo "Number of Arguments: $#"
 echo "Script name: $0"
+echo "Last Command Exit Status: $?"
 echo "Current Directory: $PWD"
 echo "User running this script: $USER"
 echo "Home directory of user: $HOME"
